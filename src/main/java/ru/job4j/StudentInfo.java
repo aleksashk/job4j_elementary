26589c977f802a6767de3dbe6c045bc65bec0e45
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-06 15:30:27 +0300

    added files

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Philimonov Aleksandr");
+        System.out.println("02.05.1984");
+    }
+}
