b197b420b5ebad8c1b24b13213870aa9edf0f8cf
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-06 15:35:15 +0300

    Checkstyle [#4123]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -4,5 +4,6 @@ public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("Philimonov Aleksandr");
         System.out.println("02.05.1984");
+        System.out.println("02.05.1984");
     }
 }
