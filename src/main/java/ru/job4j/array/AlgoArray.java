5c0da8fc62290745f98bb3bd736afed051773630
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-11 12:26:14 +0300

    Упорядочить массив. [#4441 #227165]

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -3,7 +3,6 @@ package ru.job4j.array;
 public class AlgoArray {
     public static void main(String[] args) {
         int[] array = new int[]{5, 3, 2, 1, 4};
-
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] > array[j]) {
