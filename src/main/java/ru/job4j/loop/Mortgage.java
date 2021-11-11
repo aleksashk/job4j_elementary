2afc2a52d50a6ac7d295a88d0f66bfb736c67b44
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-11 11:36:50 +0300

    corrected

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,7 +4,7 @@ public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            ++year;
+            year++;
             amount += amount * percent / 100 - salary;
         }
         return year;
