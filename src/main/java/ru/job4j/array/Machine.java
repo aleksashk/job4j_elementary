e797b2551d57191b6077eb78bc3747840a31a7bd
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-12 07:40:23 +0300

    corrected for to foreach

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -8,10 +8,10 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int balance = money - price;
-        for (int i = 0; i < coins.length; i++) {
-            while (balance - coins[i] >= 0) {
-                balance -= coins[i];
-                rsl[size] = coins[i];
+        for (int coin : coins) {
+            while (balance - coin >= 0) {
+                balance -= coin;
+                rsl[size] = coin;
                 size++;
             }
         }
