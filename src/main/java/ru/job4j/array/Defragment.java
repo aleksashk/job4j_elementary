1b1cf32fede374217b401016c7e7001659d37dfd
Aleksandr <aleksandrphilimonov@gmail.com>
2021-11-12 07:36:27 +0300

    corrected inner loop

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -4,7 +4,7 @@ public class Defragment {
     public static String[] compress(String[] array) {
         for (int index = 0; index < array.length - 1; index++) {
             if (array[index] == null) {
-                for (int i = index; i < array.length; i++) {
+                for (int i = index + 1; i < array.length; i++) {
                     if (array[i] != null) {
                         SwitchArray.swap(array, i, index);
                         break;
